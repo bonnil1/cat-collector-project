daaabe53c1e7cdcf0ffa0f8a496e1035796137e7
bonnil1 <bonnieliu759@yahoo.com>
2024-04-27 12:43:09 -0700

    add templates for home + about pages

--- catcollector_app/views.py
+++ catcollector_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    #res.send("hello") in express
+    #return HttpResponse("hello there!!!")
+    return render(request, 'cats/home.html')
+
+def about(request):
+    return render(request, 'cats/about.html')
